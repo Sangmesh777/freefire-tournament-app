0f894cea4f546b715df74893f7841e64bd428429
sangmesh mehtre <171894332+Sangmesh777@users.noreply.github.com>
2026-05-03 01:43:10 +0530

    Fix API base URL to use PC's local IP address

--- mobile/src/config.js
+++ mobile/src/config.js
@@ -1,3 +1,3 @@
 // Change this to your backend server IP/URL before running
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'http://172.31.104.108:5000';
 export const API_BASE_URL = `${BASE_URL}/api`;
